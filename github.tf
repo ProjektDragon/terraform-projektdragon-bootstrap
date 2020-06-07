78c8501a70b7fed77e96fa4da19bdd4e74306d49
Michael Simo <mpmsimo@gmail.com>
2020-06-07 16:59:40 -0700

    local and var switch

--- github.tf
+++ github.tf
@@ -32,10 +32,10 @@ resource "github_branch_protection" "bootstrap" {
   required_pull_request_reviews {
     require_code_owner_reviews = false
     dismiss_stale_reviews      = true
-    dismissal_users            = var.approved_users
+    dismissal_users            = local.approved_users
   }
 
   restrictions {
-    users = var.approved_users
+    users = local.approved_users
   }
 }
