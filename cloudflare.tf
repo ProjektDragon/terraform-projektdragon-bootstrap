6cfff1225443062cf5cf0cfa16370e44b2965ec3
Michael Simo <mpmsimo@gmail.com>
2020-06-07 19:56:05 -0700

    no need for domain name

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_record" "bootstrap" {
   count = var.cloudflare_enabled == true ? 1 : 0
 
-  zone_id = lookup(var.cloudflare_zone_ids, var.cloudflare_domain_name)
+  zone_id = var.cloudflare_zone_id
   name    = var.cloudflare_record_name
   value   = var.cloudflare_record_value
   type    = var.cloudflare_record_type
